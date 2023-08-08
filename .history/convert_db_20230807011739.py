e95700ec1b866717c098764af2ad510277f5666d
gyasis <gyasis@gmail.com>
2023-08-08 04:09:41 -0400

    added Mayo Clinic Habit Program

---
+++ .history/convert_db_20230807011739.py
@@ -0,0 +1,58 @@
+# %%
+import pandas as pd
+import sqlite3
+import re
+# %%
+def sanitize_column_name(col_name):
+    # Remove any character not a word character or a space
+    sanitized = re.sub(r'[^\w\s]', '', col_name)
+    # Replace spaces with underscores
+    return sanitized.replace(' ', '_')
+
+def df_to_sqlite(df, table_name, db_path):
+    # Sanitize column names
+    df.columns = [sanitize_column_name(col) for col in df.columns]
+    
+    # Step 1: Create a connection to SQLite database
+    conn = sqlite3.connect(db_path)
+    
+    # Step 2: Generate the CREATE TABLE SQL statement dynamically
+    create_table_sql = f"CREATE TABLE IF NOT EXISTS {table_name} ("
+    for col, dtype in df.dtypes.items():
+        if "int" in str(dtype):
+            sql_type = "INTEGER"
+        elif "float" in str(dtype):
+            sql_type = "REAL"
+        else:
+            sql_type = "TEXT"
+        create_table_sql += f"{col} {sql_type}, "
+    create_table_sql = create_table_sql.rstrip(", ") + ")"
+    
+    # Step 3: Create the table
+    with conn:
+        cursor = conn.cursor()
+        cursor.execute(create_table_sql)
+        
+    # Step 4: Insert data from the DataFrame into the SQLite table
+    df.to_sql(table_name, conn, if_exists='replace', index=False)
+    
+    conn.close()
+
+# Example usage:
+df = pd.read_csv('data/referrals.csv')
+df_to_sqlite(df, 'referrals', 'data/referrals.db')
+
+
+# %%
+# list the names of the columns of the table of the database
+conn = sqlite3.connect('data/referrals.db')
+
+
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM referrals")
+names = list(map(lambda x: x[0], cursor.description))
+print(names)
+
+# %%
+# merge two columns into one LOCATION AND LOCATIONS  KEEPING VALUES OF FIRST COLUMN UNLESS VALUES ARE EMPTY THEN DEFEAULT TO SECOND COLUMN
+curson.excute("SELECT LOCATION, LOCATIONS FROM referrals")
